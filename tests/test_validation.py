f8a4123bc4a776cb7f233a6c2fdf3177a8fe174b
Allen Goodman <allen.goodman@icloud.com>
2017-05-11 13:37:38 -0400

    refactored test_validation

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,29 +1,32 @@
 import os
-import unittest
+
+import pytest
 
 import quality.validation
 
+directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
 
-class Validation(unittest.TestCase):
-    def setUp(self):
-        self.input_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+pathname = os.path.join(directory, "BBBC006_z_aligned__a01__s1__w1_10.png")
 
-        self.input_image_path = os.path.join(self.input_directory, 'BBBC006_z_aligned__a01__s1__w1_10.png')
 
-    def testCheckDuplicateImageNameRuns(self):
-        quality.validation.check_duplicate_image_name(['/a/b.c', '/d/e.f'])
+def test_check_duplicate_image_name_runs():
+    quality.validation.check_duplicate_image_name(["/a/b.c", "/d/e.f"])
 
-    def testCheckDuplicateImageNameSameName(self):
-        with self.assertRaises(ValueError):
-            quality.validation.check_duplicate_image_name(['/a/b.c', '/a/b.c'])
 
-    def testCheckDuplicateImageNameDifferentPathAndExtension(self):
-        with self.assertRaises(ValueError):
-            quality.validation.check_duplicate_image_name(['/a/b.c', '/d/b.f'])
+def test_check_duplicate_image_name_same_name():
+    with pytest.raises(ValueError):
+        quality.validation.check_duplicate_image_name(["/a/b.c", "/a/b.c"])
 
-    def testCheckImageDimensionsRuns(self):
-        quality.validation.check_image_dimensions([self.input_image_path], 10, 10)
 
-    def testCheckImageDimensionsImageTooSmall(self):
-        with self.assertRaises(ValueError):
-            quality.validation.check_image_dimensions([self.input_image_path], 1e4, 1e4)
+def test_check_duplicate_image_name_different_path_and_extension():
+    with pytest.raises(ValueError):
+        quality.validation.check_duplicate_image_name(["/a/b.c", "/d/b.f"])
+
+
+def test_check_image_dimensions_runs():
+    quality.validation.check_image_dimensions([pathname], 10, 10)
+
+
+def test_check_image_dimensions_image_too_small():
+    with pytest.raises(ValueError):
+        quality.validation.check_image_dimensions([pathname], 1e4, 1e4)
